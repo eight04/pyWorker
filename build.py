6d1296de81b0b1d4913d6f389d0eeeabe9709ef5
eight <eight04@gmail.com>
2015-06-07 08:57:43 +0800

    Add -A option to git add command

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ version = settings["version"]
 os.system("py setup.py sdist bdist_wheel")
 os.system("twine upload dist/*")
 os.system("rm -R dist")
-os.system("git add .")
+os.system("git add -A .")
 os.system("git commit -m 'Release v{}'".format(version))
 os.system("git tag -a v{} -m 'Release v{}'".format(version, version))
 os.system("git push --follow-tags")
