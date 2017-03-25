40a7b95ce3e3955c1aa72b46391c16d9c4b7c566
eight <eight04@gmail.com>
2017-03-26 07:24:48 +0800

    Update pyxcute to 0.3.1

--- cute.py
+++ cute.py
@@ -1,13 +1,14 @@
-# pylint: disable=line-too-long
+#! python3
+
 from xcute import cute, Bump, Version, Exc
 
 cute(
+    pkg_name = 'worker',
     lint = 'pylint worker cute docs/conf.py setup test',
     test = ['lint', 'python test.py', 'readme_build'],
     bump_pre = 'test',
-    bump = Bump('worker/__init__.py'),
     bump_post = ['dist', 'release', 'publish', 'install'],
-    dist = 'python setup.py sdist bdist_wheel',
+    dist = ['rm -r build dist', 'python setup.py sdist bdist_wheel'],
     release = [
         'git add .',
         'git commit -m "Release v{version}"',
@@ -19,11 +20,10 @@ cute(
     ],
     install = 'pip install -e .',
     install_err = 'elevate -c -w pip install -e .',
-    readme_build = 'python setup.py --long-description > %temp%/ld && rst2html --no-raw --exit-status=1 --verbose %temp%/ld %temp%/ld.html',
+    readme_build = 'mkdir build & python setup.py --long-description > build/ld && rst2html --no-raw --exit-status=1 --verbose build/ld build/ld.html',
     readme_build_err = ['readme_show', Exc()],
     readme_show = 'start %temp%/ld.html',
     readme = 'readme_build',
     readme_post = 'readme_show',
-    version = [Version('worker/__init__.py'), 'echo {version}'],
     doc = 'sphinx-autobuild -B -z worker docs docs/build'
 )
