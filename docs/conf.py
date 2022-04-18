5ed1cba4b3168badd5d49a3af3ad82dc9793763c
eight04 <eight04@gmail.com>
2022-04-19 00:10:10 +0800

    Add: readthedocs config

--- docs/conf.py
+++ docs/conf.py
@@ -9,12 +9,12 @@ from worker import __version__
 project = "pyThreadWorker"
 author = "eight04 (https://github/eight04)"
 
-r = run("git tag --points-at HEAD", capture_output=True, encoding="utf8")
+r = run("git tag --points-at HEAD", capture_output=True, encoding="utf8", check=True)
 is_tagged = r.stdout.strip()
 if is_tagged:
     version = __version__
 else:
-    r = run("git rev-parse HEAD", capture_output=True, encoding="utf8")
+    r = run("git rev-parse HEAD", capture_output=True, encoding="utf8", check=True)
     version = f"{__version__}+ ({r.stdout.strip()[:6]})"
 release = version
 
